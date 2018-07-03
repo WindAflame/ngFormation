723e27476fa452c9fbda6b84aa7a9e872e49778e
Charles <>
2018-07-03 16:48:23 +0200

    adding ngw bootstrap

--- src/app/bets/bet-form-template/bet-form-template.component.ts
+++ src/app/bets/bet-form-template/bet-form-template.component.ts
@@ -1,5 +1,6 @@
 import { Bet } from './../../models/bet';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-bet-form-template',
@@ -13,9 +14,13 @@ export class BetFormTemplateComponent implements OnInit {
   newbet: EventEmitter<Bet> = new EventEmitter<Bet>();
 
 
-  constructor() { }
-
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { }
   ngOnInit() {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
   public submit() {
--- src/app/bets/bets.module.ts
+++ src/app/bets/bets.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BetFormTemplateComponent } from './bet-form-template/bet-form-template.component';
 import { BetFormCodeComponent } from './bet-form-code/bet-form-code.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
   declarations: [
     BetComponent,
